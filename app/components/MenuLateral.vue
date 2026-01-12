131a9ae5239192131d3eeeb1d222714eea1e9d9b
Igor Fronza <igorfronza20@gmail.com>
2026-01-12 12:41:27 -0300

    (improv) - Adicionado fontawasome e ajustando menu lateral

--- app/components/MenuLateral.vue
+++ app/components/MenuLateral.vue
@@ -7,21 +7,21 @@
             </svg>
         </button>
 
-        <transition enter-active-class="transition-transform duration-300" enter-from-class="-translate-x-full" enter-to-class="translate-x-0" leave-active-class="transition-transform duration-300" leave-from-class="translate-x-0" leave-to-class="-translate-x-full">
+        <transition enter-active-class="transition-all ease-in-out duration-300" enter-from-class="-translate-x-full" enter-to-class="translate-x-0" leave-active-class="transition-transform duration-300" leave-from-class="translate-x-0" leave-to-class="-translate-x-full">
             <div v-if="isOpen" class="fixed inset-0 z-70 flex">
                 <!-- overlay: blur + darken -->
-                <div class="absolute inset-0 bg-black/40 backdrop-blur-sm" @click.self="close" aria-hidden="true"></div>
+                <div class="absolute inset-0 bg-black/40 backdrop-blur-sm transition-opacity duration-500 ease-out" :class="opacity" @click.self="close" aria-hidden="true"></div>
 
                 <!-- menu (left) -->
                 <aside class="relative z-50 w-80 max-w-[92vw] h-full bg-gradient-to-b from-white to-gray-400 rounded-r-2xl p-5 shadow-2xl flex flex-col" role="dialog" aria-modal="true">
                     <div class="flex items-center justify-between mb-3">
                         <h2 class="text-lg font-semibold">Menu</h2>
-                        <Button @click="close" label="X" class="w-auto h-[35px]" color="var(--menu-button)" />
+                        <Button @click="close" label="<i class='fa-solid fa-xmark'></i>" class="w-auto h-[35px]" color="var(--menu-button)" />
                     </div>
 
                     <nav class="flex flex-col gap-[5px]" aria-label="Menu principal">
                         <div v-for="(item, i) in itemsList" :key="i" class="flex flex-col gap-2 items-baseline">
-                            <NuxtLink :to="item?.to" class="text-[var(--menu-button)] font-medium flex-1 flex gap-4">
+                            <NuxtLink :to="item?.to" class="text-[var(--menu-button)] w-full font-medium flex-1 flex gap-4 p-2 hover:shadow-md duration-150 rounded-lg" @click="isOpen = false">
                                 <span v-html="item?.icon"></span>
                                 {{ item?.label }}
                             </NuxtLink>
@@ -43,9 +43,25 @@ const itemsList = [
         label: 'Início',
         icon: '<i class="fa-solid fa-user"></i>',
     },
+    {
+        to: '/sucess',
+        label: 'Sucesso',
+        icon: '<i class="fa-solid fa-check-circle"></i>',
+    },
 ]
 
 const isOpen = ref(false)
+const opacity = ref('opacity-0')
+
+watch(isOpen, (value) => {
+    if (value) {
+        setTimeout(() => {
+            opacity.value = 'opacity-100'
+        }, 300) // 1s de delay
+    } else {
+        opacity.value = 'opacity-0'
+    }
+})
 
 function toggle() {
     isOpen.value = !isOpen.value
@@ -56,6 +72,4 @@ function close() {
 }
 </script>
 
-<style>
-
-</style>
+<style></style>
